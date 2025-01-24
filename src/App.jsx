9c3c5f0390b53e140953c39d306dadf8be755882
sumit <sumitbagate3@gmail.com>
2025-01-24 11:36:09 +0530

    Revert "Merge branch 'main' of https://github.com/thunderboltz-18/ToDoBLOG"
    This reverts commit ff2a2d3218ce9a99693c67425fd8aec828de3309, reversing
    changes made to a98ac61160f8e03b0a992b7514b2bd14f996bddd.
    
    
    qa!
    
    +
    +

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Us
 import "./App.css";
 import Login from "./Components/login";
 import Feed from "./Components/Home";
-import Navbar from "./Components/navbar";
+import Navbar from "./Components/Navbar";
 function App() {
 
   const[isAuth,setIsAuth] = React.useState(localStorage
